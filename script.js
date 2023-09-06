47d8db29a4ecb03f3e5af59a06ed44c4cf3ebce8
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-06 17:07:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let number = parseInt(prompt())
+let factorial = 1;
+for (let i = 1; i <= number; i++) {
+	factorial*=i;
+}
+
+alert(`The factorial of 5 is ${factorial}`)
